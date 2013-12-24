87e96b3120c3acc2aaec4d8c56cebde2865c2a6e
edipotrebol <edipotrebol@gmail.com>
2013-12-24 12:35:38 +0100

    Adding new action for editing dataset (construct + destruct)

--- sql/xlyre.constructor.sql
+++ sql/xlyre.constructor.sql
@@ -52,13 +52,15 @@ UNLOCK TABLES;
 -- Actions
 LOCK TABLES `Actions` WRITE;
 INSERT INTO `Actions`(`IdAction`, `IdNodeType`, `Name`, `Command`, `Icon`, `Description`,`Sort`, `Module`,`Multiple`, `Params`) VALUES ( 7501, 4000 ,"Delete Catalog", "deletecatalog", "delete_section.png","Deletes the current Open Data Catalog", 97, 'xlyre', 0, NULL);
-INSERT INTO `Actions`(`IdAction`, `IdNodeType`, `Name`, `Command`, `Icon`, `Description`,`Sort`, `Module`,`Multiple`, `Params`) VALUES ( 7502,  4000,"Create New Dataset", "createdataset", "change_next_state.png","Creates a new Dataset", 99, 'xlyre', 0, NULL);
+INSERT INTO `Actions`(`IdAction`, `IdNodeType`, `Name`, `Command`, `Icon`, `Description`,`Sort`, `Module`,`Multiple`, `Params`) VALUES ( 7502,  4000,"Create New Dataset", "managedataset", "change_next_state.png","Creates a new Dataset", 99, 'xlyre', 0, NULL);
+INSERT INTO `Actions`(`IdAction`, `IdNodeType`, `Name`, `Command`, `Icon`, `Description`,`Sort`, `Module`,`Multiple`, `Params`) VALUES ( 7503,  4001,"Edit Dataset", "managedataset", "change_next_state.png","Edit an existing Dataset", 99, 'xlyre', 0, NULL);
 UNLOCK TABLES;
 
 -- Roles for these actions
 LOCK TABLES `RelRolesActions` WRITE;
 INSERT INTO RelRolesActions VALUES (NULL,201,7501,0,1,3);
 INSERT INTO RelRolesActions VALUES (NULL,201,7502,0,1,3);
+INSERT INTO RelRolesActions VALUES (NULL,201,7503,0,1,3);
 UNLOCK TABLES;
 
 -- XlyreCatalog Table
