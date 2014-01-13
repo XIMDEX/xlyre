b546926ac4dfad9d05e3cdae61b58d5e8532c0ce
edipotrebol <edipotrebol@gmail.com>
2014-01-13 16:27:41 +0100

    Extending delete_dataset function to be used in other levels + fixing minor warnings

--- actions/deletedataset/Action_deletedataset.class.php
+++ actions/deletedataset/Action_deletedataset.class.php
@@ -61,10 +61,15 @@ class Action_deletedataset extends ActionAbstract {
 		$this->render($values, null, 'default-3.0.tpl');
 	}
 
-	function delete_dataset() {
 
-		$idNode	= $this->request->getParam("nodeid");
-	    
+	/*
+	 * If the function receives a $dataset, this action will not use $this->request
+	 * and it will not render any template
+	 */
+	function delete_dataset($dataset = -1) {
+
+		$idNode	= ($dataset != -1) ? $dataset : $this->request->getParam("nodeid");
+	 
 	    /*
 		 * TODO:
 		 * Delete all dependencies (distributions)
@@ -78,27 +83,34 @@ class Action_deletedataset extends ActionAbstract {
 		$parentID = $node->get('IdParent');
 
 		$node = new Node($idNode);
-		$node->delete();
-		$err = NULL;
-		if($node->numErr) {
-			$err = _("An error occurred while deleting:");
-			$err .= '<br>' . $node->get('IdNode') . " " . $node->GetPath() . '<br>' . _("Error message: ") . $node->msgErr . "<br><br>";
+		$result = $node->delete();
+
+		if ($dataset == -1) {
+			$err = NULL;
+			if($node->numErr) {
+				$err = _("An error occurred while deleting:");
+				$err .= '<br>' . $node->get('IdNode') . " " . $node->GetPath() . '<br>' . _("Error message: ") . $node->msgErr . "<br><br>";
+			}
+
+			if (strlen($err)) {
+				$this->messages->add($err, MSG_TYPE_ERROR);
+			} else {
+				$this->messages->add(_("The dataset were successfully deleted"), MSG_TYPE_NOTICE);
+			}
+				
+			$this->reloadNode($parentID);
+
+			$values = array(
+				'messages' => $this->messages->messages,
+				'action_with_no_return' => true,
+			);
+
+			$this->render($values, NULL, 'messages.tpl');
+		}
+		else {
+			return $result;
 		}
 
-		if (strlen($err)) {
-			$this->messages->add($err, MSG_TYPE_ERROR);
-		} else {
-			$this->messages->add(_("The dataset were successfully deleted"), MSG_TYPE_NOTICE);
-		}
-			
-		$this->reloadNode($parentID);
-
-		$values = array(
-			'messages' => $this->messages->messages,
-			'action_with_no_return' => true,
-		);
-
-		$this->render($values, NULL, 'messages.tpl');
 	}
 
 }
