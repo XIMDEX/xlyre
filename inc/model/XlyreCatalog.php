4b3e6c703056f58ffef5748a2bd222797db86b00
edipotrebol <edipotrebol@gmail.com>
2014-03-04 12:56:56 +0100

    Publish DCAT in cascade

--- inc/model/XlyreCatalog.php
+++ inc/model/XlyreCatalog.php
@@ -26,6 +26,7 @@
 
 ModulesManager::file('/inc/model/orm/XlyreCatalog_ORM.class.php', 'xlyre');
 ModulesManager::file('/inc/model/XlyreDataset.php', 'xlyre');
+ModulesManager::file('/inc/nodetypes/xlyreopendatasectionmetadata.inc', 'xlyre');
 
 class XlyreCatalog extends XlyreCatalog_ORM {
     
@@ -75,6 +76,76 @@ class XlyreCatalog extends XlyreCatalog_ORM {
             return $stringxml;
         }
     }
+
+
+    /**
+     * Export catalog info to its RDF format
+     * @return string A string that contains RDF/XML file
+     */
+    public function ToRdf() {
+        
+        $node = new Node($this->IdCatalog);
+
+        $childrens = $node->GetChildren(XlyreOpenDataSectionMetadata::IDNODETYPE);
+        $defaultPublicationChannel = $this->getDefaultPublicationChannel();
+
+        $stringrdf = "<rdf:RDF";
+        $stringrdf .= " xmlns:time='http://www.w3.org/2006/time#'";
+        $stringrdf .= " xmlns:dct='http://purl.org/dc/terms/'";
+        $stringrdf .= " xmlns:dc='http://purl.org/dc/elements/1.1/'";
+        $stringrdf .= " xmlns:dcat='http://www.w3.org/ns/dcat#'";
+        $stringrdf .= " xmlns:foaf='http://xmlns.com/foaf/0.1/'";
+        $stringrdf .= " xmlns:xsd='http://www.w3.org/2001/XMLSchema#'";
+        $stringrdf .= " xmlns:rdfs='http://www.w3.org/2000/01/rdf-schema#'";
+        $stringrdf .= " xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>";
+
+        $stringrdf .= "<dcat:Catalog rdf:about='@@@RMximdex.pathtoabs(".$childrens[0].",".$defaultPublicationChannel.")@@@'>";
+
+        $stringrdf .= "<dct:identifier>@@@RMximdex.pathtoabs(".$childrens[0].",".$defaultPublicationChannel.")@@@</dct:identifier>";
+
+        # Add these lines for each language (not implemented for catalog yet)
+        // <dct:title xml:lang='es'>Title</dct:title>
+        // <dct:description xml:lang='es'>Decription</dct:description>
+        // <dc:language>IsoName</dc:language>
+        
+        # Add these props (not implemented for catalog yet)
+        // <dct:publisher rdf:resource="http://" />
+        // <dct:extent>
+        //     <dct:SizeOrDuration>
+        //         <rdf:value rdf:datatype="http://www.w3.org/2001/XMLSchema#nonNegativeInteger">
+        //             74
+        //         </rdf:value>
+        //     </dct:SizeOrDuration>
+        // </dct:extent>
+        // <dct:spatial rdf:resource="http://datos.gob.es/recurso/sector-publico/territorio/pais/Espana" />
+        // <dcat:themeTaxonomy rdf:resource="http://datos.gob.es/kos/sector-publico/sector/" />
+        // <dct:license rdf:resource="https://sede.minetur.gob.es/es-ES/Paginas/aviso.aspx#uso" />
+        
+        $format = 'Y-m-d\TH:i:s\Z';
+        $stringrdf .= "<dct:issued rdf:datatype='http://www.w3.org/2001/XMLSchema#dateTime'>".date($format, $this->Issued)."</dct:issued>";
+        $stringrdf .= "<dct:modified rdf:datatype='http://www.w3.org/2001/XMLSchema#dateTime'>".date($format, $this->Modified)."</dct:modified>";
+        $stringrdf .= "<foaf:homepage rdf:resource='@@@RMximdex.sectionpathabs(".$this->IdCatalog.")@@@' />";
+
+        $node = new Node($this->IdCatalog);
+        $datasets = $node->GetChildren(XlyreOpenDataset::IDNODETYPE);
+        foreach ($datasets as $value) {
+            $dataset = new XlyreDataset($value);
+            $stringrdf .= $dataset->ToRdf();
+        }
+
+        $stringrdf .= "</dcat:Catalog>";
+        $stringrdf .= "</rdf:RDF>";
+
+        return $stringrdf;
+    }
+
+
+    private function getDefaultPublicationChannel(){
+        
+        return 10001;
+
+    }
+
     
 }
 ?>
